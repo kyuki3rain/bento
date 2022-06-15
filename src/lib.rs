966e7d4e56e6c935afe33c4e18806f1a0d479459
kyuki3rain <kyuki3rain>
2022-06-16 01:37:56 +0900

    add_benchmark

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![deny(rust_2018_idioms)]
+
+#[macro_use]
+#[cfg(test)]
+extern crate counted_array;
+
+pub mod ast;
+pub mod builtin;
+pub mod environment;
+pub mod evaluator;
+pub mod lexer;
+pub mod object;
+pub mod parser;
+pub mod repl;
+pub mod token;
